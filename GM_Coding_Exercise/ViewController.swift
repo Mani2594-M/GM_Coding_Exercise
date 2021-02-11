d96932e09e6272accaa52a172075f76b80a037b9
Mani <manikanteshiosdev@gmail.com>
2021-02-11 11:51:29 -0600

    Initial Commit

---
+++ GM_Coding_Exercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GM_Coding_Exercise
+//
+//  Created by Mani V on 2/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
