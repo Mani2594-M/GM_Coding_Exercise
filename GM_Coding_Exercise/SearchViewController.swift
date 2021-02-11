f8925b57016bde325ac6141941b457f5be46e1c2
Mani <manikanteshiosdev@gmail.com>
2021-02-11 13:06:47 -0600

    Implemented SearchBar and table view delegates

--- GM_Coding_Exercise/SearchViewController.swift
+++ GM_Coding_Exercise/SearchViewController.swift
@@ -9,12 +9,35 @@ import UIKit
 
 class SearchViewController: UIViewController {
     
+    var searchResults = [String]()
+    
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 }
 
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("The search text is: \(searchBar.text!)")
+    }
+}
+
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
